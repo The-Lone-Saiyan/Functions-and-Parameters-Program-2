f804ada4023fd841ea8f5a97f08854c30dab434d
Roberto Fitzhugh <rgf274@email.vccs.edu>
2024-02-19 15:17:00 -0500

    In Progress

--- Functions and Parameters Program 2.cpp
+++ Functions and Parameters Program 2.cpp
@@ -4,13 +4,8 @@ using namespace std;
 
 double CurrentPrice, LastYearPrice, TwoYearsAgoPrice;
 
-double InflationFormula(double &P1, double &P2, double &P3)
-{
-	double Inflation1 = ((P2 - P1) / P2);
-	double Inflation2 = ((P3 - P2) / P3);
-
-	return Inflation1, Inflation2;
-}
+double InflationFormula1(double& P1, double& P2);
+double InflationFormula2(double& P2, double& P3);
 
 int main()
 {
@@ -34,4 +29,18 @@ int main()
 	cout << "\nInflation Over Two Years: " << endl;
 
 	return 0;
+}
+
+double InflationFormula1(double& P1, double& P2)
+{
+	double Inflation1 = ((P2 - P1) / P2);
+
+	return Inflation1;
+}
+
+double InflationFormula2(double& P2, double& P3)
+{
+	double Inflation2 = ((P3 - P1) / P3);
+
+	return Inflation2;
 }
