9b98869e48a2e74123f57af67e567868e9f84232
Roberto Fitzhugh <rgf274@email.vccs.edu>
2024-02-19 15:01:54 -0500

    In Progress

--- Functions and Parameters Program 2.cpp
+++ Functions and Parameters Program 2.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 double CurrentPrice, LastYearPrice, TwoYearsAgoPrice;
 
+double InflationFormula(double &P1, double &P2, double &P3)
+{
+	double Inflation1 = ((P2 - P1) / P2);
+	double Inflation2 = ((P3 - P2) / P3);
+
+	return Inflation1, Inflation2;
+}
+
 int main()
 {
 	cout << "Please enter the current item price: ";
@@ -14,6 +22,16 @@ int main()
 
 	cout << "\nPleast enter the price from two years ago: ";
 		cin >> TwoYearsAgoPrice;
+
+	cout << "Current Price: " << CurrentPrice << endl;
+
+	cout << "\nPrice one year ago: " << LastYearPrice << endl;
+
+	cout << "\nPrice two years ago: " << TwoYearsAgoPrice << endl;
+
+	cout << "\nCurrent inflation: " << endl;
+
+	cout << "\nInflation Over Two Years: " << endl;
 
 	return 0;
 }
