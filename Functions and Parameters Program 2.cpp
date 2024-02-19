ba61873f2cbc55b3b47ce1c14d8e7123c218bc5a
Roberto Fitzhugh <rgf274@email.vccs.edu>
2024-02-19 14:48:30 -0500

    In Progress

--- Functions and Parameters Program 2.cpp
+++ Functions and Parameters Program 2.cpp
@@ -2,9 +2,18 @@
 
 using namespace std;
 
+double CurrentPrice, LastYearPrice, TwoYearsAgoPrice;
+
 int main()
 {
-	cout << "Please enter the current item price: \n";
+	cout << "Please enter the current item price: ";
+		cin >> CurrentPrice;
+
+	cout << "\nPlease enter the price from a year ago: ";
+		cin >> LastYearPrice;
+
+	cout << "\nPleast enter the price from two years ago: ";
+		cin >> TwoYearsAgoPrice;
 
 	return 0;
 }
